1d60e2c7c70b6fcf10680418326a5f0111fbf43b
gersonLevi <keylayerins@gmail.com>
2023-10-03 17:03:35 -0500

    modulo 2 terminado

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './assets/css/base/base.css';
 import './assets/css/componentes/card.css'
 import Home from './pages/Home';
+import Page404 from './pages/Page404';
 import Sobre from './pages/Sobre.jsx';
+import Header from './components/Header';
+
 
 import { BrowserRouter as Router , Routes, Route } from "react-router-dom";
 
@@ -10,9 +13,12 @@ function App() {
   return (
     <>
     <Router>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/sobre' element={<Sobre />} />
+        <Route path='/*' element={<Page404 />} />
+
       </Routes>
     </Router>    
     </>
